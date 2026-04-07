e416aa39f0a2a7091d00109a04ee2fbab094ca1d
unknown <yash.savant@polynomial.ai>
2026-04-08 00:53:47 +0530

    added signup logout apis

--- src/module/auth/auth.conttroller.ts
+++ src/module/auth/auth.conttroller.ts
@@ -6,12 +6,23 @@ import { AuthService } from './auth.service';
 @Public() // ✅ make all routes in this controller public by default
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: AuthService){
-    
-  }
+  constructor(private readonly userService: AuthService){}
   
   @Post('login')
-  login(@Body() {email , password}:{email:string , password:string}) {
-    return this.userService.validateUser(email ,password)
+  login(@Body() {email , password}:{email:string , password:string} ,@Req() req:any) {
+    const user =  this.userService.validateUser(email ,password)
+    req.session.user = user;
+    return 'Login successfully!'
+  }
+
+  @Post('signup')
+  signup(@Body() {email,password ,name}:{email:string , password:string , name:string}){
+    return this.userService.registerUser(email , password, name)
+  }
+
+  @Post('logout')
+  logout(@Req() req: any) {
+    req.session.destroy(() => {});
+    return { message: 'Logged out' };
   }
 }
